9004fdd327396a571f45c4ecba99e3f17df0713a
dobanthony <dobanthony38@gmail.com>
2025-11-21 13:48:43 +0800

    nice

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            // Add role column: possible values - customer, admin, delivery
+            $table->string('role')->default('customer'); 
+            
             $table->rememberToken();
             $table->timestamps();
         });
